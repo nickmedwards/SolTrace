6de9a1064399b5f8ed43cac526b7782b4856c645
nick <nick.edwards2341@gmail.com>
2026-05-12 15:53:17 -0500

    added writing code to simdriver/main

--- coretrace/simdriver/main.cpp
+++ coretrace/simdriver/main.cpp
@@ -531,7 +531,11 @@ int main(int argc, char *argv[])
         try
         {
             auto t_write_start = std::chrono::steady_clock::now();
+<<<<<<< HEAD
             result.write_group_json_file(output_file + ".json");
+=======
+            result.write_json_file(output_file + ".json");
+>>>>>>> a05360f (added writing code to simdriver/main)
             auto t_write_end = std::chrono::steady_clock::now();
             std::cout << "  Written in "
             << std::chrono::duration<double>(t_write_end - t_write_start).count()
