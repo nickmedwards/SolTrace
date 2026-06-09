a00e817106974809314813afdf038e62e530cea2
nmedwards2 <nick.edwards2341@gmail.com>
2026-06-09 15:36:11 -0500

    convert to std::set

--- coretrace/simulation_runner/optix_runner/OptixCSP/src/core/soltrace_system.h
+++ coretrace/simulation_runner/optix_runner/OptixCSP/src/core/soltrace_system.h
@@ -143,11 +143,6 @@ namespace OptixCSP
         /// exactly m_number_of_rays hit rays are returned.  Enabled by default.
         void set_trim_excess_rays(bool trim) { m_trim_excess_rays = trim; }
         bool get_trim_excess_rays() const { return m_trim_excess_rays; }
-        void set_groups(const std::vector<int32_t>& groups) { my_groups = groups; }
-        size_t get_num_groups() const { return my_groups.size(); }
-        void set_groups(const std::vector<int32_t>& groups) { m_groups = groups; }
-        size_t get_num_groups() const { return m_groups.size(); }
-        int32_t get_group(int32_t element_id);
 
     private:
         // could use FRIEND_TEST macro, however to avoid linking gtest to prod, forward declare test class and make it a friend
